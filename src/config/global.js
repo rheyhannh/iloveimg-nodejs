9028ab7e23c89bda6db4a1dee20643e71dc8b610
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-02-18 06:44:09 +0700

    feat: Add project config variables

---
+++ src/config/global.js
@@ -0,0 +1,17 @@
+export default {
+	/**
+	 * ILoveApi URL that should match to JWT `iss` on authentication token
+	 * - e.g. `api.ilovepdf.com`
+	 */
+	ILOVEIMG_API_URL: 'api.ilovepdf.com',
+	/**
+	 * ILoveApi URL protocol
+	 * - e.g. `https`
+	 */
+	ILOVEIMG_API_URL_PROTOCOL: 'https',
+	/**
+	 * ILoveApi version
+	 * - e.g. `v1`
+	 */
+	ILOVEIMG_API_VERSION: 'v1'
+};
